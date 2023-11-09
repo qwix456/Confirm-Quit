eb2ca967c9fae50224bc8c41d8201852470b09b1
skyue <104967297+Skyue1@users.noreply.github.com>
2023-11-09 11:52:39 +0900

    Update main.cpp

--- jni/src/main.cpp
+++ jni/src/main.cpp
@@ -16,28 +16,14 @@ void PlayLayerNew::Quit() {
   return PlayLayerNew::onQuit(playlayer);
 }
 
-//this is where all your hooks should be
 void ApplyHooks() {
     HOOK("_ZN9PlayLayer6onQuitEv", PlayLayerNew::onQuitHooks, PlayLayerNew::onQuit);
 }
 
-//this is where your starting patches should be
-void ApplyPatches() {
-    PatchManager tmp;
-
-    tmp.addPatch("libcocos2dcpp.so", 0x20CDD8, "00 BF");
-
-    //make sure to apply the patches
-    tmp.Modify();
-}
-
-//this function will run when the app starts
 void loader() {
     ApplyHooks();
-    ApplyPatches();
 }
 
-//starter
 JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved)
 {
     loader();
